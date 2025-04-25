46bd774038d52de34b7010f7a57c8a4a042df3d8
YunJuHyung <wngud5289@naver.com>
2025-04-25 17:23:05 +0900

    chore: file setup

---
+++ cinema-seat-bandit/cinema-seat-bandit/View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cinema-seat-bandit
+//
+//  Created by 윤주형 on 4/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+}
+
