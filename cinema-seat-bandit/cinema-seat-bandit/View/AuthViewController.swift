2a8aaa7465b7b30bd7fc140e980911a7fac775d4
ksyq12 <ksyq12@daum.net>
2025-04-27 23:32:51 +0900

    feat: 로그인/회원가입 뷰 구현

--- cinema-seat-bandit/cinema-seat-bandit/View/AuthViewController.swift
+++ cinema-seat-bandit/cinema-seat-bandit/View/AuthViewController.swift
@@ -9,10 +9,27 @@ import UIKit
 import SnapKit
 
 final class AuthViewController: UIViewController {
+    private let authView = AuthView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupViews()
+        setupConstraints()
     }
-
+    
+    private func setupViews() {
+        view.backgroundColor = .white
+        
+        [authView].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    private func setupConstraints() {
+        authView.snp.makeConstraints {
+            $0.top.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+    }
 }
