dfed1cbefe3a3442b187e0e9df7d8901de33a49b
YunJuHyung <wngud5289@naver.com>
2025-05-01 19:55:41 +0900

    feat: mvvm패턴 구현 완료

--- cinema-seat-bandit/cinema-seat-bandit/View/MovieListViewController.swift
+++ cinema-seat-bandit/cinema-seat-bandit/View/MovieListViewController.swift
@@ -3,6 +3,13 @@ import SnapKit
 
 final class MovieListViewController: UIViewController {
 
+    let movieListVM = MovieListViewModel()
+
+    private var popularMovies: [Movie] = []
+    private var upcomingMovies: [Movie] = []
+
+    private let popularSection = MovieSectionView(title: "Popular")
+    private let upcomingSection = MovieSectionView(title: "Upcoming")
 
     private let scrollView: UIScrollView = {
         let scrollView = UIScrollView()
@@ -20,57 +27,6 @@ final class MovieListViewController: UIViewController {
         return stackView
     }()
 
-    private let popularSection = MovieSectionView(title: "Popular")
-    private let upcomingSection = MovieSectionView(title: "Upcoming")
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        setupViews()
-        setupConstraints()
-        loadTrendingData()
-        loadUpcommingData()
-        setupClickEvents()
-
-    }
-
-    private func loadTrendingData() {
-        NetworkManager.shared.request(api: .trending()) {
-            (result: Result<TrendingResponse, Error>) in
-            switch result {
-            case .success(let response):
-                self.popularSection.setMovies(response.results)
-            case .failure(let error):
-                print("에러 발생: \(error)")
-            }
-        }
-    }
-
-    private func loadUpcommingData() {
-        NetworkManager.shared.request(api: .upcoming()) {
-            (result: Result<UpcommingResponse, Error>) in
-            switch result {
-            case .success(let response):
-                self.upcomingSection.setMovies(response.results)
-            case .failure(let error):
-                print("에러 발생: \(error)")
-            }
-        }
-    }
-
-    private func setupClickEvents() {
-        popularSection.onMovieTapped = { [weak self] movie in
-            let detailVC = MovieDetailViewController()
-            detailVC.movie = movie
-            self?.navigationController?.pushViewController(detailVC, animated: true)
-        }
-
-        upcomingSection.onMovieTapped = { [weak self] movie in
-            let detailVC = MovieDetailViewController()
-            detailVC.movie = movie
-            self?.navigationController?.pushViewController(detailVC, animated: true)
-        }
-    }
     private func setupViews() {
         view.addSubview(scrollView)
         scrollView.addSubview(stackView)
@@ -89,4 +45,107 @@ final class MovieListViewController: UIViewController {
             $0.width.equalToSuperview()
         }
     }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupViews()
+        setupConstraints()
+        setupDelegates()
+        bindOnClickedEvents()
+        movieListVM.updateTrendingMovie()
+        movieListVM.updateUpcomingMovies()
+
+    }
+
+    private func setupDelegates() {
+        popularSection.collectionView.delegate = self
+        popularSection.collectionView.dataSource = self
+        upcomingSection.collectionView.delegate = self
+        upcomingSection.collectionView.dataSource = self
+    }
+
+    private func bindOnClickedEvents() {
+        movieListVM.trendingMovieUpdated = { [weak self] movies in
+            self?.popularMovies = movies
+            self?.popularSection.collectionView.reloadData()
+            self?.scrollToMiddle(collectionView: self?.popularSection.collectionView)
+        }
+
+        movieListVM.upcomingMovieUpdated = { [weak self] movies in
+            self?.upcomingMovies = movies
+            self?.upcomingSection.collectionView.reloadData()
+            self?.scrollToMiddle(collectionView: self?.upcomingSection.collectionView)
+        }
+    }
+
+
+
+    func scrollToMiddle(collectionView: UICollectionView?) {
+        guard let collectionView = collectionView else { return }
+        let middleIndexPath = IndexPath(item: popularMovies.count / 2, section: 0)
+        collectionView.scrollToItem(at: middleIndexPath, at: .centeredHorizontally, animated: false)
+    }
 }
+
+
+// MARK: - UICollectionViewDataSource, delegate
+
+extension MovieListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == popularSection.collectionView {
+            return popularMovies.count
+        } else {
+            return upcomingMovies.count
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieCell.identifier, for: indexPath) as? MovieCell else {
+            return UICollectionViewCell()
+        }
+
+        let movie = (collectionView == popularSection.collectionView)
+        ? popularMovies[indexPath.item]
+        : upcomingMovies[indexPath.item]
+
+        cell.configure(with: movie, showVoteAverage: true)
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+        let movieItems = (collectionView == popularSection.collectionView)
+        ? popularMovies[indexPath.item]
+        : upcomingMovies[indexPath.item]
+
+        let detailVC = MovieDetailViewController()
+        detailVC.movie = movieItems
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+
+
+}
+
+extension MovieListViewController: UICollectionViewDelegate {
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+
+        guard let collectionView = scrollView as? UICollectionView else { return }
+
+        let visibleItems = collectionView.indexPathsForVisibleItems
+        guard let currentIndex = visibleItems.sorted().first?.item else { return }
+
+        let count = (collectionView == popularSection.collectionView)
+        ? popularMovies.count
+        : upcomingMovies.count
+
+        if currentIndex <= count {
+            let newIndex = count / 2 + currentIndex
+            collectionView.scrollToItem(at: IndexPath(item: newIndex, section: 0), at: .centeredHorizontally, animated: false)
+        } else if currentIndex >= count * 2 {
+            let newIndex = count / 2 + (currentIndex % count)
+            collectionView.scrollToItem(at: IndexPath(item: newIndex, section: 0), at: .centeredHorizontally, animated: false)
+        }
+    }
+}
+
--- cinema-seat-bandit/cinema-seat-bandit/View/MovieSectionView.swift
+++ cinema-seat-bandit/cinema-seat-bandit/View/MovieSectionView.swift
@@ -10,38 +10,26 @@ import SnapKit
 
 final class MovieSectionView: UIView {
 
-    var movies: [Movie] = []
-    var onMovieTapped: ((Movie) -> Void)?
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 16)
+        return label
+    }()
 
-
-    func scrollToMiddle() {
-        let middleIndexPath = IndexPath(item: movies.count / 2, section: 0)
-        collectionView.scrollToItem(at: middleIndexPath, at: .centeredHorizontally, animated: false)
-    }
-
-    private let titleLabel = UILabel()
-
-    private lazy var collectionView: UICollectionView = {
-        let collectionView = UICollectionView(
-            frame: .zero,
-            collectionViewLayout: createLayout()
-        )
-
+    lazy var collectionView: UICollectionView = {
+        let layout = createLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.register(MovieCell.self, forCellWithReuseIdentifier: MovieCell.identifier)
-        collectionView.dataSource = self
         return collectionView
     }()
+
 
     init(title: String) {
         super.init(frame: .zero)
         titleLabel.text = title
-        titleLabel.font = .boldSystemFont(ofSize: 20)
         setupViews()
         setupConstraints()
-        collectionView.delegate = self
-        collectionView.dataSource = self
-
     }
 
     required init?(coder: NSCoder) {
@@ -51,17 +39,6 @@ final class MovieSectionView: UIView {
     private func setupViews() {
         [titleLabel, collectionView]
             .forEach{ addSubview($0)}
-    }
-
-    func setMovies(_ movies: [Movie]) {
-        self.movies = Array(repeating: movies, count: 3).flatMap { $0 }
-        collectionView.reloadData()
-
-        DispatchQueue.main.async { [weak self] in
-            guard let self = self else { return }
-            self.layoutIfNeeded() //화면이 전활 될 때도 호출이됨
-            self.scrollToMiddle()
-        }
     }
 
 
@@ -79,7 +56,7 @@ final class MovieSectionView: UIView {
         }
     }
 
-    private func createLayout() -> UICollectionViewCompositionalLayout {
+    func createLayout() -> UICollectionViewCompositionalLayout {
         let itemSize = NSCollectionLayoutSize(
             widthDimension: .fractionalWidth(1.0),
             heightDimension: .fractionalHeight(1.0)
@@ -114,58 +91,6 @@ final class MovieSectionView: UIView {
                    }
                }
            }
-
         return UICollectionViewCompositionalLayout(section: section)
     }
 }
-
-// MARK: - UICollectionViewDataSource
-
-extension MovieSectionView: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return movies.count
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieCell.identifier, for: indexPath) as? MovieCell else {
-            return UICollectionViewCell()
-        }
-        cell.configure(with: movies[indexPath.item], showVoteAverage: true)
-        return cell
-    }
-
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let selectedMovie = movies[indexPath.item]
-        onMovieTapped?(selectedMovie)
-    }
-
-
-}
-
-extension MovieSectionView: UICollectionViewDelegate {
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        adjustCurrentIndexIfNeeded()
-    }
-
-    private func adjustCurrentIndexIfNeeded() {
-        let visibleItems = collectionView.indexPathsForVisibleItems
-        guard let currentIndex = visibleItems.sorted().first?.item else { return }
-
-        let itemCount = movies.count
-        let realItemCount = movies.count
-
-        if currentIndex <= realItemCount {
-            // 너무 앞으로 왔다
-            let newIndex = itemCount / 2 + currentIndex
-            let newIndexPath = IndexPath(item: newIndex, section: 0)
-            collectionView.scrollToItem(at: newIndexPath, at: .centeredHorizontally, animated: false)
-        } else if currentIndex >= itemCount - realItemCount {
-            // 너무 뒤로 갔다
-            let newIndex = itemCount / 2 + (currentIndex % realItemCount)
-            let newIndexPath = IndexPath(item: newIndex, section: 0)
-            collectionView.scrollToItem(at: newIndexPath, at: .centeredHorizontally, animated: false)
-        }
-    }
-}
-
-
